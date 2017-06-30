56bf932056887c22a6749bada5cc606147952d82
Dan McClory <danmcclory@gmail.com>
2017-06-30 10:10:10 -0400

    add an example script. also ohmygodwerebackagain

---
+++ example.rb
@@ -0,0 +1,33 @@
+
+require 'pry'
+require 'mystery'
+
+# tracer = Mystery.new(path: "foo.rb")
+
+tracer_explicit = Mystery.new(
+  path: "example.rb",
+  method: :awesome,
+  event_names: ['call']
+)
+
+class Foo
+  def awesome(x)
+    puts "called Foo.awesome!"
+    500 +x
+  end
+end
+
+puts 'starting the trace!'
+tracer_explicit.start!
+f = Foo.new
+f.awesome(100)
+f.awesome(200)
+f.awesome(300)
+f.awesome(700)
+puts 'stopping the trace!'
+tracer_explicit.stop!
+puts '------------------'
+puts f.awesome(480)
+
+
+# binding.pry
